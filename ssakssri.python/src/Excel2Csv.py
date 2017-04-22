2dbf63e80323bd13359c78ffa1fff0ab8bc2b80b
ssakssri <ssakssri@gmail.com>
2017-04-22 22:10:14 +0900

    Excel2Csv

---
+++ ssakssri.python/src/Excel2Csv.py
@@ -0,0 +1,29 @@
+'''
+Created on 2017. 4. 22.
+
+@author: ssakssri
+'''
+import xlrd
+import csv
+
+def csv_from_excel():
+
+    wb = xlrd.open_workbook('/Users/ssakssri/Downloads/picklist.xlsx')
+    sh = wb.sheet_by_name('csv')
+    your_csv_file = open('/Users/ssakssri/Downloads/picklist.csv', 'w', encoding='utf-8')
+    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+    for rownum in range(sh.nrows):
+        wr.writerow(localize_floats(sh.row_values(rownum)))
+
+    your_csv_file.close()
+
+
+def localize_floats(row):
+    return [
+        str(el).replace('.0', '') if isinstance(el, float) else el 
+        for el in row
+    ]
+
+
+csv_from_excel()
